3b33b03b02ae94622af9007dec88fd6aa34fe36d
TsangHingTat <125679650+TsangHingTat@users.noreply.github.com>
2023-09-12 01:18:50 +0000

    update

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Get a reference to the video element
 const video = document.getElementById('myVideo');
 
-// Add an event listener for when the video ends
 video.addEventListener('ended', function() {
-    // When the video ends, rewind it to the beginning and play it again
     video.currentTime = 0;
     video.play();
 });
